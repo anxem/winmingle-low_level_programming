5a7c662a6db8cb03832b0d81d6795421bc9b6b0b
cobilor <anssville@gmail.com>
2026-03-16 13:24:37 +0100

    Add 4-puts file that prints exactly a text with new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Author: Chibuzor ANselm Obilor
+ * Program: WinMingle Community C Training
+ * Description: Prints a line using the puts function
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return 0;
+}
