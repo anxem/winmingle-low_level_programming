cafdd369084b769e93a9879b0b432c54d82511ff
cobilor <anssville@gmail.com>
2026-03-16 12:18:00 +0100

    Add file that run a C file through the preprocessor and save in c file

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Author: Chibuzor Anselm Obilor
+ * Program: WinMingle Community c Training
+ * Description: Simple c program used to test the preprocessor script
+ */
+
+int main(void)
+{
+    Printf("Hello, WinMingle!\n");
+    return 0;
+}
