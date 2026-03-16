916561529fad5fc468bc81a97010cc4361b5c2e8
cobilor <anssville@gmail.com>
2026-03-16 13:35:40 +0100

    Add 5-printf.c that prints with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Author: Chibuzor Anselm Obilor
+ * Program: WinMingle Community C Training
+ * Description: Prints a sentence using the printf function
+ */
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return 0;
+}
