5e8e08c712c88c5f6532aec8cb806962f7859dde
cobilor <anssville@gmail.com>
2026-03-17 17:20:51 +0100

    Add 6-abs that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Author: Chibuzor Anselm Obilor
+ * Program: WinMingle Community C Training
+ * Description: Computes the absolute value of an integer
+ */
+
+int _abs(int n)
+{
+    if (n < 0)
+        return (-n);
+
+    return (n);
+}
+
+
