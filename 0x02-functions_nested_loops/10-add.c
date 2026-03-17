48abf2beb20c7f66460349f2e6669b5a12375f94
cobilor <anssville@gmail.com>
2026-03-17 17:51:08 +0100

    Add 10-add that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Author: Chibuzor Anselm Obilor
+ * Program: WinMingle Community C Training
+ * Description: Adds two integers and returns the result
+ */
+
+int add(int a, int b)
+{
+    return (a + b);
+}
+
