752ab88b7548540e9496c196b07300e2143cb1c2
cobilor <anssville@gmail.com>
2026-03-18 10:01:52 +0100

    Add 0-main that checks positive or negative after debugging the code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Author: Chibuzor Anselm Obilor
+ * Program: WinMingle Community C Training
+ * Description: Testing the function positive_or_negative() using 0 as input
+ */
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+    int i;
+ 
+    i = 0;
+    positive_or_negative(i);
+ 
+    return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * Author: Your Full Name
+ * Program: WinMingle Community C Training
+ * Description: Header file for Debugging and code correction.
+ */
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
+
