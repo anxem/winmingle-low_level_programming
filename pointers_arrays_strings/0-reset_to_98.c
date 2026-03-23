9762096b2eae6e05d588be253d37f50389d251aa
cobilor <anssville@gmail.com>
2026-03-23 15:29:47 +0100

    Add README and rest_to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Author: Chibuzor Anselm Obilor
+ * Program: WinMingle Community C Training
+ * Description: Updates the value a pointer point to to 98.
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
+
