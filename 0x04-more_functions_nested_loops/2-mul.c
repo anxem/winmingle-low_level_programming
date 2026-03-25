e3988c921e6b24c32f0162bdad9d596c484f81b9
cobilor <anssville@gmail.com>
2026-03-25 16:19:31 +0100

    Add 2-mul that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Author: Chibuzor Anselm Obilor
+ * Program: WinMingle Community C Training
+ * Description: Multiplies two integers
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
